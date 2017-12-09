b7212ec0acecc6b210bec077ffe7056ca2402c7f
Anshul Agrawal <anshulagrawal35@gmail.com>
2017-12-09 13:20:07 -0800

    check if stripe customer was created, not sources specifically

--- controller/utils.go
+++ controller/utils.go
@@ -20,7 +20,7 @@ func IsUserAllowedToOrder(user *auth.User, visit *visits.MerchantVisit) (bool, s
 		return true, "ok"
 	}
 
-	if user.CustomerProfile.StripeCustomer == nil || user.CustomerProfile.StripeCustomer.Sources.ListMeta.Count == 0{
+	if user.CustomerProfile.StripeCustomer == nil || user.CustomerProfile.StripeCustomer.ID == ""{
 		//No Stripe Customer
 		return false, "noCard"
 	}
